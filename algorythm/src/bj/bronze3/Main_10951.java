14b5e8827735a52b3777935c13f1137357391aea
DESKTOP-PQ4OC50\Tree <treegrammer@gmail.com>
2021-01-07 19:50:41 +0900

    BJ Bronze3 A+B_4

--- algorythm/src/bj/bronze3/Main_10951.java
+++ algorythm/src/bj/bronze3/Main_10951.java
@@ -1,10 +1,10 @@
+package bj.bronze3;
 import java.util.Scanner;
 
-public class Main_10950 {
+public class Main_10951 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		for(int i=0; i<t; i++) {
+		while(sc.hasNext()) {
 			int a = sc.nextInt();
 			int b = sc.nextInt();
 			System.out.println(a+b);

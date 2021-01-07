09df98dccea44ba8638f9adc88f18a401d7142f3
DESKTOP-PQ4OC50\Tree <treegrammer@gmail.com>
2021-01-07 22:33:11 +0900

    BJ_Bronze5 10171 CAT

---
+++ algorythm/src/bj/bronze5/Main_10171.java
@@ -0,0 +1,10 @@
+package bj.bronze5;
+
+public class Main_10171 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}

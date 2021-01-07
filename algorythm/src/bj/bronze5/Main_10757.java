8e4e5630ab2fa3631821c85001d402c11ad77080
DESKTOP-PQ4OC50\Tree <treegrammer@gmail.com>
2021-01-08 00:08:18 +0900

    BJ_Bronze5 10757 큰수A+B

---
+++ algorythm/src/bj/bronze5/Main_10757.java
@@ -0,0 +1,13 @@
+package bj.bronze5;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main_10757 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger a = new BigInteger(sc.next());
+		BigInteger b = new BigInteger(sc.next());
+		System.out.println(a.add(b));
+	}
+}

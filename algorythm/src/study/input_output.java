d84ac240f1c44c862f99d94b63d00388424f022d
DESKTOP-PQ4OC50\Tree <treegrammer@gmail.com>
2021-01-09 02:21:16 +0900

    about input and output

--- algorythm/src/study/input_output.java
+++ algorythm/src/study/input_output.java
@@ -12,7 +12,7 @@ public class input_output{
 	
 	public static void main(String[] args) throws IOException {
 		
-// input -> Scanner, BufferedReader
+// input -> Scanner < BufferedReader
 	
 	// Scanner : 버퍼사이즈 1024
 	Scanner sc = new Scanner(System.in);	// 선언
@@ -28,7 +28,7 @@ public class input_output{
 	StringTokenizer st = new StringTokenizer(br_line);							// 파싱위한 선언(띄어쓰기 기준 자르기)
 	int br_num = Integer.parseInt(st.nextToken());								// 파싱
 	
-// output -> println, BufferedWriter
+// output -> println < BufferedWriter
 	
 	// println : println -> print -> write() + newLine() 순서로 처리
 	System.out.println("println");
@@ -41,25 +41,25 @@ public class input_output{
 	bw.newLine();	// 줄바꿈
 	bw.close();		// 버퍼 닫기
 	
-// 문자열 포맷팅 -> String, StringBuilder, StringBuffer
+// 문자열 포맷팅 -> String < StringBuffer < StringBuilder
 	
 	// String : 내부적으로 boxing과정 거쳐 느림 -> immutable(불변) 객체로, 연산 시 새로운 객체를 생성하는 과정을 거쳐 느림
 	String a = "";
 	String b = "";
 	String c = a + b;
 	
-	// StringBuilder : mutable한 객체로, boxing과정을 거치지 않아 속도적으로 빠르게 사용 가능
-	StringBuilder sb = new StringBuilder();
-	sb.append("String\n");
-	sb.append("Builder\n");
-	System.out.println(sb);
-	
 	// StringBuffer : thread-safe 특징
 	StringBuffer sf = new StringBuffer();
 	sf.append("String\n");
 	sf.append("Buffer\n");
 	System.out.println(sf);
-	
+
+	// StringBuilder : mutable한 객체로, boxing과정을 거치지 않아 속도적으로 빠르게 사용 가능
+	StringBuilder sb = new StringBuilder();
+	sb.append("String\n");
+	sb.append("Builder\n");
+	System.out.println(sb);
+
 	}
 	
 }

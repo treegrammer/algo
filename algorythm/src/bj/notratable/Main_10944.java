c89ac327c8beaa89e04a5883857e493d92308003
DESKTOP-PQ4OC50\Tree <treegrammer@gmail.com>
2021-01-09 05:06:50 +0900

    notratable 10944 random

---
+++ algorythm/src/bj/notratable/Main_10944.java
@@ -0,0 +1,10 @@
+package bj.notratable;
+
+import java.util.Random;
+
+public class Main_10944 {
+	public static void main(String[] args) {
+		Random random = new Random();
+		System.out.println(random.nextInt(9999)+1);
+	}
+}

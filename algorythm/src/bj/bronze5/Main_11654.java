12d131e002451fc8f10c4e543b0cb30e7db50642
DESKTOP-PQ4OC50\Tree <treegrammer@gmail.com>
2021-01-13 17:55:08 +0900

    BJ_Bronze5 11654 아스키코드

---
+++ algorythm/src/bj/bronze5/Main_11654.java
@@ -0,0 +1,11 @@
+package bj.bronze5;
+
+import java.util.Scanner;
+
+public class Main_11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int result = sc.next().charAt(0);
+		System.out.println(result);
+	}
+}

805e2bf3682699aeabbca4e3aa5c56da0fa67907
DESKTOP-PQ4OC50\Tree <treegrammer@gmail.com>
2021-01-27 08:34:00 +0900

    BJ_Bronze 1085 직사각형에서 탈출

---
+++ algorythm/src/bj/bronze3/Main_1085.java
@@ -0,0 +1,21 @@
+package bj.bronze3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1085 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer tokens = new StringTokenizer(br.readLine());
+		int x = Integer.parseInt(tokens.nextToken());
+		int y = Integer.parseInt(tokens.nextToken());
+		int w = Integer.parseInt(tokens.nextToken());
+		int h = Integer.parseInt(tokens.nextToken());
+		
+		int ck1 = x<w-x?x:w-x;
+		int ck2 = y<h-y?y:h-y;
+		int result = ck1<ck2?ck1:ck2;
+		System.out.println(result);
+	}
+}

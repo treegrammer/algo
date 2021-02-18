20c19b10e76f41f4349aade4d2e40b7985933217
DESKTOP-PQ4OC50\Tree <treegrammer@gmail.com>
2021-02-18 10:29:40 +0900

    BJ_Bronze5 2338 긴자리 계산

---
+++ algorythm/src/bj/bronze5/Main_2338.java
@@ -0,0 +1,21 @@
+package bj.bronze5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+
+public class Main_2338 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		BigInteger a = new BigInteger(br.readLine());
+		BigInteger b = new BigInteger(br.readLine());
+		
+		System.out.println(a.add(b));
+		System.out.println(a.subtract(b));
+		System.out.println(a.multiply(b));
+	}
+}
